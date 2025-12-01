475f44afe13f1798b680549e78276f32063e4895
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2025-12-01 14:43:33 +0200

    Fix package build and duplicates

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ rollupTypes: false })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
