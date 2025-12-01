05912f64c3d530f08b2c43ca24f6a952e6b9d90a
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2025-12-01 14:38:52 +0200

    Configure package for release

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'GitPackage',
+      fileName: 'git-package',
+    },
+  },
+})
